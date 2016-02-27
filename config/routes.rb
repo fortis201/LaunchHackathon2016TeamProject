607907e61656d25a981d5190514000ce6eb5e561
Michael Campos <camposmj89@gmail.com>
2016-02-27 00:04:58 -0800

    Set up the CRUD for vendors

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     get 'payment' => 'users#payment'
   end
 
+  resources :vendors
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
